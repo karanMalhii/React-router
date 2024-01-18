bf2d901a35af3ff5b0409d2e49cf47bd2be73cad
karanmalhii <karanmalhii1313@gmail.com>
2024-01-18 22:13:28 +0530

    file setup done

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./pages/About.jsx";
+import Home from "./pages/Home.jsx";
+import Contact from "./pages/Contact.jsx";
+const router = createBrowserRouter([
+  {
+    path: "about",
+    element: <About />,
+  },
+  {
+    // path: "",
+    index:true,
+    element: <Home />,
+  },
+  {
+    path: "contact",
+    element: <Contact />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </React.StrictMode>
+);
