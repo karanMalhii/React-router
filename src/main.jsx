3633cbf23960585f8d3198835f5ac80b72106002
karanmalhii <karanmalhii1313@gmail.com>
2024-01-23 22:22:11 +0530

    Templete (Outlet)

--- src/main.jsx
+++ src/main.jsx
@@ -6,20 +6,28 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import About from "./pages/About.jsx";
 import Home from "./pages/Home.jsx";
 import Contact from "./pages/Contact.jsx";
+import Layout from "./layout/layout.jsx";
 const router = createBrowserRouter([
   {
-    path: "about",
-    element: <About />,
+    element:<Layout/>,
+    children:[
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "",
+        // index:true,
+        element: <Home />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+
+    ]
   },
-  {
-    // path: "",
-    index:true,
-    element: <Home />,
-  },
-  {
-    path: "contact",
-    element: <Contact />,
-  },
+ 
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
